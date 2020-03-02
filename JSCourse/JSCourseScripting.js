29e41ac1868b23dd5002aaa76e01dec5dac11917
rss838 <60705939+rss838@users.noreply.github.com>
2020-03-02 09:47:46 -0800

    Add files via upload

---
+++ JSCourse/JSCourseScripting.js
@@ -0,0 +1,27 @@
+//This Script contains a bunch of comments for learning/reference purposes!
+
+//This goes at the beginning of a scripting file for "reasons??<insert actual reason here at some point!"
+"use strict";
+
+// This is a comment
+
+//Brings up browset alert box, also JS does addition
+alert(3+5+2);
+
+let message; //"Let" createsa JS variable
+
+message = 'Hola'; //= adds content to the variable//
+
+alert(message); //shows variable content
+
+const MY_SANITY = 0 // Const is a way of creating a variable that can't/won't be changed
+/*constants that are hard coded, and pre-know
+should always be capitalized. My sanity is 0, I know that it
+is 0, and I also know that it will always be 0. So, Therefore,
+It should be a capitalized constant.
+There are also other constants as well, constants that are
+unchanging, but not always known or not known until run time are
+not capitalized. Example:
+const pageLoadTime = 1231232131;
+constants also don't necesarily need to be defined immediatly
+*/ 
