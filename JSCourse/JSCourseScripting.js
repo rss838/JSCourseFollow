ab65c3453c6232062bfb4bdda18a80390a338cea
rss838 <60705939+rss838@users.noreply.github.com>
2020-03-06 08:57:29 -0800

    Add files via upload

--- JSCourse/JSCourseScripting.js
+++ JSCourse/JSCourseScripting.js
@@ -106,3 +106,90 @@ let value1 = true;
 alert(typeof value1); //boolean
 value1 = String(value1);
 alert(typeof value1); //string
+
+//number conversions Numbers are automatically converted for lots of stuff, but not all stuff...
+//true and false become 1 and 0, respectively
+let stringNumber = "123"
+stringNumber = Number(stringNumber)
+alert(typeof stringNumber)
+
+
+//boolean conversion, empty stuff like 0, or undefined become false, other stuff become true
+alert ( Boolean(1) ); //true
+
+//operand, or arguement. What operations are applied to. 5 * 2 has two operands, 5, and 2
+
+//an operator is unary if it has a single operand
+//an operator is binary if it has two operands
+
+
+//string concatenation, binary +
+//when binary plus is applied to strings, it merges them
+let s = "my" + "string"
+alert(s); //mystring
+//if one operand is a string, the other one gets converted to a string as well
+alert('1' + 2); //"12"
+
+//string concatenation only happens for binary +. other arithemetic
+//operators work only with numbers and always convert
+//their operands to numbers
+
+//numeric conversion unary +
+//when and unary operator has + applied to it it gets converted to a number
+alert( +true ); //1
+
+/* remainder %: the result of integer division
+*/
+alert(6 % 2 ) //3
+
+//exponentiation: means exactly what it sounds like..
+alert( 2 ** 2); //4 (2 * 2)
+//exponentation can work for non integer numbers as well..
+alert( 4 ** 0.5); //2
+
+//increment/decrement: increases or decreases a VARIABLE by 1
+let counter = 2;
+counter++;        // works the same as counter = counter + 1, but is shorter
+alert( counter ); // 3
+
+counter = 2;
+--counter;        // works the same as counter = counter - 1, but is shorter
+alert( counter ); // 1
+
+
+/* Modify-in-place. Applies an operator to a variable,
+and stores the new result in that same variable.*/
+//instead of:
+let p = 2;
+p = p + 5;
+//use
+let p = 2;
+p+= 5; // n now equals 7, but it took slightly less long to type
+
+
+//operators exercises
+let a = 1, b = 1;
+let c = ++a; 2 ?
+let d = b++; 1 ?
+
+let a = 2;
+let x = 1 + (a *= 2);
+//a=4
+//x=5
+
+//type conversions exercise
+"" + 1 + 0 //1 wrong. should be 10
+"" - 1 + 0 //-1
+true + false //1
+6 / "3" //2
+"2" * "3" //6
+4 + 5 + "px" //9px
+"$" + 4 + 5 //$45
+"4" - 2 //2
+"4px" - 2 //NaN
+7 / 0 // 0 Wrong. correct answer is infinity
+"  -9  " + 5 //-95
+"  -9  " - 5 //-14
+null + 1 //1
+undefined + 1 //1 Wrong, correct answer is NaN
+" \t \n" - 2 // -2 
